6aa1ee4de39e69b280d823fd13e89748f9708765
manu <emmanuel.castanier@gmail.com>
2014-11-06 11:59:46 +0100

    removed auto gen header

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: manu
- * Date: 11/6/14
- * Time: 11:23 AM
- * To change this template use File | Settings | File Templates.
- */
+
 public class Application {
     public static void main(String argv[]){
         FirstClass first = new FirstClass("first");
