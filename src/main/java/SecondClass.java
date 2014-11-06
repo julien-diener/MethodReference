89f4534de938e8aa20465aa30d6722e719754aae
manu <emmanuel.castanier@gmail.com>
2014-11-06 12:04:32 +0100

    rm useless cast

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -3,6 +3,6 @@ import java.util.function.Function;
 
 public class SecondClass {
     public String applyFunction(String name, Function<String,String> function){
-        return (String) function.apply(name);
+        return function.apply(name);
     }
 }
